c36346ec19f87dd51cb8b02ccd525a0698e5a700
devchansoria01 <devchansoria19@gmail.com>
2026-01-17 01:00:01 +0530

    resume lnk

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
 
                             <a 
                                 target="_blank"
-                                href="#https://drive.google.com/file/d/1_jcJaUtmTBD0UY26V8XhLtEhNlGjCI1L/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                                href="https://drive.google.com/file/d/1_jcJaUtmTBD0UY26V8XhLtEhNlGjCI1L/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download Resume
                             </a>
                         </div>
