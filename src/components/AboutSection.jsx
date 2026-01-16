9f51988f433f52b4ffa25976a70ae1835c9a4ce8
devchansoria01 <devchansoria19@gmail.com>
2026-01-17 00:47:16 +0530

    resume link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,9 @@ export const AboutSection = () => {
                                 Get in Touch
                             </a>
 
-                            <a href="#contact" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a 
+                                target="_blank"
+                                href="#https://drive.google.com/file/d/1_jcJaUtmTBD0UY26V8XhLtEhNlGjCI1L/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download Resume
                             </a>
                         </div>
